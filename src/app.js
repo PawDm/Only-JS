5f31c94695137837af9e23d6bc97d2c71abbb8cf
Dmitry <Jirnixu@yandex.ru>
2020-11-12 09:44:55 +0500

    add app

---
+++ src/app.js
@@ -0,0 +1,63 @@
+import { authWithEmailAndPassword, getAuth } from './auth';
+import { Question } from './question';
+import './styles.css';
+import { createModal, isValid } from './utils';
+const form = document.getElementById('form');
+const modalBtn = document.getElementById('modal-btn');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+window.addEventListener('load', Question.renderList);
+
+form.addEventListener('submit', submitFormHandler);
+
+modalBtn.addEventListener('click', openModal);
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value);
+});
+function submitFormHandler(event) {
+  event.preventDefault();
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    };
+
+    // Async request to server to save question
+    Question.create(question).then(() => {
+      submitBtn.disabled = true;
+      input.className = '';
+      submitBtn.disabled = false;
+    });
+
+    //console.log('Question: ', question);
+    input.value = '';
+  }
+}
+
+function openModal() {
+  createModal('Авторизация', getAuth());
+  document
+    .getElementById('auth-form')
+    .addEventListener('submit', authFormHandler, { once: true });
+}
+
+function authFormHandler(event) {
+  event.preventDefault();
+  const btn = event.target.querySelector('button');
+  const email = event.target.querySelector('#email').value;
+  const password = event.target.querySelector('#password').value;
+  btn.disabled = true;
+  authWithEmailAndPassword(email, password)
+    .then(Question.fetch)
+    .then(renderModelAfterAuth)
+    .then(() => (btn.disabled = false));
+}
+
+function renderModelAfterAuth(content) {
+  if (typeof content === 'string') {
+    createModal('Ошибка!', content);
+  } else {
+    createModal('Список вопросов', Question.listToHTML(content));
+  }
+}
